424af51ff0b72183d17d70d4ee474eda700f9158
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-01-17 00:29:02 +0100

    Add decorate_self_attributes
    Return self.decorate, but use self attributes for default values

--- barython/widgets/base.py
+++ barython/widgets/base.py
@@ -10,12 +10,12 @@ class Widget():
     """
     #: cache the content after update
     _content = None
-    #: padding
-    padding = 0
     #: background for the widget
     bg = None
     #: foreground for the widget
     fg = None
+    #: padding
+    padding = 0
     #: list of fonts index used
     fonts = tuple()
     #: refresh rate
@@ -28,6 +28,11 @@ class Widget():
     def decorate(self, text, fg=None, bg=None, padding=0, font=None):
         """
         Decorate a text with custom properties
+
+        :param fg: foreground
+        :param bg: background
+        :param padding: padding around the text
+        :param font: index of font to use
         """
         return (9*"{}").format(
             "%{{B{}}}".format(bg) if fg else "",
@@ -41,13 +46,35 @@ class Widget():
             "%{B-}" if bg else ""
         )
 
-    def update(self, panel):
-        panel.update()
+    def decorate_with_self_attributes(self, text, *args, **kwargs):
+        """
+        Return self.decorate but uses self attributes for default values
+        """
+        d_kwargs = {
+            "fg": self.fg, "bg": self.bg, "padding": self.padding,
+            "font": self.fonts[0] if self.fonts else None, **kwargs
+        }
+        for parameter, value in zip(("fg", "bg", "padding", "font"), args):
+            d_kwargs[parameter] = value
 
-    def __init__(self, bg=None, fg=None, fonts=None, refresh=0):
+        return self.decorate(text, **d_kwargs)
+
+    def _update_panel(self, panel, new_content):
+        """
+        If content has changed, request the panel update
+        """
+        if self._content != new_content:
+            self._content = new_content
+            panel.update()
+
+    def update(self, panel):
+        pass
+
+    def __init__(self, bg=None, fg=None, padding=0, fonts=None, refresh=0):
         self.bg = bg if bg else self.bg
         self.fg = fg if fg else self.fg
         self.fonts = fonts if fonts else self.fonts
+        self.padding = padding if padding else self.padding
         self.refresh = refresh if refresh else self.refresh
 
 
@@ -55,20 +82,29 @@ class TextWidget(Widget):
     text = ""
 
     def update(self, panel):
-        def thread_update():
-            while True:
-                new_content = self.decorate(
-                    self.text, fg=self.fg, bg=self.bg, padding=self.padding,
-                    font=self.fonts[0] if self.fonts else None
-                )
-                if self._content != new_content:
-                    self._content = new_content
-                    panel.update()
-                time.sleep(self.refresh)
+        new_content = self.decorate(
+            self.text, fg=self.fg, bg=self.bg, padding=self.padding,
+            font=self.fonts[0] if self.fonts else None
+        )
+        self._update_panel(panel, new_content)
 
+    def start(self, panel):
         if self.refresh == 0:
             self.refresh = panel.refresh
-        t = threading.Thread(target=thread_update)
+        t = threading.Thread(target=self.update)
+        t.start()
+
+    def __init__(self, text, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text = text if text else self.text
+
+
+class ThreadWidget(Widget):
+
+    def start(self, panel):
+        if self.refresh == 0:
+            self.refresh = panel.refresh
+        t = threading.Thread(target=self.update)
         t.start()
 
     def __init__(self, text, *args, **kwargs):
