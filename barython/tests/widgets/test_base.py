73c08238d2c5bc2f37b455eb37009f0b931581f9
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-01-17 00:29:42 +0100

    Add tests for widgets

--- barython/tests/widgets/test_base.py
+++ barython/tests/widgets/test_base.py
@@ -6,7 +6,7 @@ from barython.widgets.base import Widget
 
 
 def test_base_widget_construction():
-    Widget()
+    Widget(fg="#FFFF11", bg="#FF9021", padding=1, fonts=[1, 2], refresh=2)
 
 
 def test_base_widget_decorate_simple():
@@ -41,3 +41,31 @@ def test_base_widget_decorate():
         )
     )
     assert decorated_text == expected_result
+
+
+def test_base_widget_decorate_self_attributes_empty():
+    """
+    Test decorate_with_self_attributes without any parameter
+    """
+    w = Widget()
+    kwargs = {
+        "text": "test", "fg": "#FFFF11", "bg": "#FF9021", "font": 1,
+        "padding": 2,
+    }
+
+    assert w.decorate_with_self_attributes(**kwargs) == w.decorate(**kwargs)
+
+
+def test_base_widget_decorate_self_attributes():
+    """
+    Test decorate_with_self_attributes without any parameter
+    """
+    fg = "#FFFF11"
+    bg = "#FF9021"
+    padding = 2
+    kwargs = {"fg": fg, "bg": bg, "padding": padding}
+
+    w = Widget(fonts=[1, ], **kwargs)
+
+    assert (w.decorate_with_self_attributes("test") ==
+            w.decorate("test", font=1, **kwargs))
