f82bd0362db3374271bd141f168e0cf423855c58
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-01-17 16:53:39 +0100

    Add add_widget to screen

--- barython/screen.py
+++ barython/screen.py
@@ -12,6 +12,23 @@ class Screen():
     refresh = 0.1
     _widgets = {"l": list(), "c": list(), "r": list()}
 
+    def add_widget(self, alignment, *widgets, index=None):
+        """
+        Add a widget to a screen
+
+        :param alignment: where adding the widget (left, center, right)
+        :param *widgets: widgets to add
+        :param index: if set, will insert the widgets before the specified
+                      index (default: None)
+        """
+        if index is None:
+            self._widgets[alignment].extend(widgets)
+        else:
+            list_widgets = self._widgets[alignment]
+            self._widgets[alignment] = (
+                list_widgets[:index] + list(widgets) + list_widgets[index:]
+            )
+
     def draw(self):
         pass
 
--- barython/tests/test_screen.py
+++ barython/tests/test_screen.py
@@ -2,8 +2,39 @@
 import pytest
 
 from barython.screen import Screen
+from barython.widgets.base import Widget
 
 
 def test_screen():
     s = Screen()
     s.run()
+
+
+def test_screen_add_widget():
+    s = Screen()
+    w = Widget()
+
+    s.add_widget("l", w)
+    assert s._widgets["l"][0] == w
+
+    s.add_widget("c", w)
+    assert s._widgets["c"][0] == w
+
+    s.add_widget("r", w)
+    assert s._widgets["r"][0] == w
+
+
+def test_screen_add_widget_insert():
+    """
+    Test to add widget before specified index
+    """
+    s = Screen()
+    w = Widget()
+    s.add_widget("l", w)
+
+    w1 = Widget()
+    w2 = Widget()
+    s.add_widget("l", w1, w2, index=0)
+
+    assert s._widgets["l"][0] == w1
+    assert s._widgets["l"][1] == w2
