3a518093df770122c17a103361d3396625293834
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-01-17 02:15:27 +0100

    Safe import for widgets
    Thanks to qtile for this function

---
+++ barython/widgets/__init__.py
@@ -0,0 +1,33 @@
+import logging
+import traceback
+import importlib
+
+
+logger = logging.getLogger("barython")
+
+
+def safe_import(module_name, class_name):
+    """
+    try to import a module, and if it fails because an ImporError
+    it logs on WARNING, and logs the traceback on DEBUG level
+
+    Thanks for qtile for this function
+    """
+    if type(class_name) is list:
+        for name in class_name:
+            safe_import(module_name, name)
+        return
+    package = __package__
+    # python 3.2 don't set __package__
+    if not package:
+        package = __name__
+    try:
+        module = importlib.import_module(module_name, package)
+        globals()[class_name] = getattr(module, class_name)
+    except ImportError as error:
+        msg = "Can't Import Widget: '%s.%s', %s"
+        logger.warning(msg % (module_name, class_name, error))
+        logger.debug(traceback.format_exc())
+
+
+safe_import(".date", "Date")
