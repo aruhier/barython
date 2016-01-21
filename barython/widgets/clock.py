42f790812c1a5d380260555b1f2895b71a8d3b91
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-01-21 01:57:20 +0100

    First batch of clock widget

---
+++ barython/widgets/clock.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import time
+
+from .base import ThreadedWidget
+
+
+class ClockWidget(ThreadedWidget):
+    date_format = "%c"
+
+    def update(self, *args, **kwargs):
+        while True:
+            new_content = self.decorate_with_self_attributes(
+                datetime.now().strftime(self.date_format)
+            )
+            super().update(new_content=new_content, *args, **kwargs)
+            time.sleep(self.refresh)
+
+    def __init__(self, date_format=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.date_format = (
+            self.date_format if date_format is None else date_format
+        )
