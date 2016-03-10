296db67ec411c1212cab0a95904b05f00cf73fba
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-03-10 23:27:49 +0100

    ClockWidget: clean useless code
    Continous update was already implemented in the parent class

--- barython/widgets/clock.py
+++ barython/widgets/clock.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-import time
 
 from .base import ThreadedWidget
 
 
 class ClockWidget(ThreadedWidget):
-    def continuous_update(self):
-        while True and not self._stop.is_set():
-            self.update()
-            time.sleep(self.refresh)
-
     def organize_result(self, date_now, **kwargs):
         return super().organize_result(date_now.strftime(self.date_format))
 
